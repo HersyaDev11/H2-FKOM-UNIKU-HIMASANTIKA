55a42fcfdf7bf5d87c128383b1a69538f14f6d4f
Hersya Yudina <145486775+HersyaDev11@users.noreply.github.com>
2026-09-01 01:11:40 +0700

    page.tsx

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,6 +4,7 @@ import QuotesSection from "./components/QuotesSection/QuotesSection";
 import PeminatanSection from "./components/PeminatanSection/PeminatanSection";
 import GallerySection from "./components/GallerySection/GallerySection";
 import TestimonialsSection from "./components/TestimonialsSection/TestimonialsSection";
+import Footer from "./components/Footer/Footer";
 import Chatbot from "./components/Chatbot/Chatbot";
 
 export default function Home() {
@@ -26,6 +27,8 @@ export default function Home() {
 
       {/* 5. Section Testimoni Alumni Reel (Paling Akhir Tetap) */}
       <TestimonialsSection />
+
+      <Footer />
 
       <Chatbot />
     </main>
