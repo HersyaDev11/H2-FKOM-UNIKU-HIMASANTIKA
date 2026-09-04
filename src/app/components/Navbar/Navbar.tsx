a6c8a19408f7b617798afaf589e63e97f50e5c47
Hersya Yudina <145486775+HersyaDev11@users.noreply.github.com>
2026-09-04 08:13:08 +0700

    Navbar.tsx

--- src/app/components/Navbar/Navbar.tsx
+++ src/app/components/Navbar/Navbar.tsx
@@ -11,10 +11,10 @@ import { useLanguage } from '../../context/LanguageContext';
 
 const NAV_LINKS = [
   { id: 'beranda', idLabel: 'Beranda', enLabel: 'Home', href: '#beranda' },
-  { id: 'fakta', idLabel: 'Keunggulan', enLabel: 'Excellence', href: '#fakta-akademik' },
-  { id: 'peminatan', idLabel: 'Peminatan', enLabel: 'Specializations', href: '#peminatan' },
-  { id: 'galeri', idLabel: 'Galeri', enLabel: 'Gallery', href: '#galeri' },
-  { id: 'testimoni', idLabel: 'Alumni', enLabel: 'Alumni', href: '#testimoni-alumni' },
+  { id: 'fakta', idLabel: 'Keunggulan', enLabel: 'Excellence', href: 'fakta-akademik' },
+  { id: 'peminatan', idLabel: 'Peminatan', enLabel: 'Specializations', href: 'peminatan' },
+  { id: 'galeri', idLabel: 'Galeri', enLabel: 'Gallery', href: 'galeri' },
+  { id: 'testimoni', idLabel: 'Alumni', enLabel: 'Alumni', href: 'testimoni-alumni' },
 ];
 
 export default function Navbar() {
